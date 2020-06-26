4ce2de1e94f21464fee51087e1143392f7c89b30
Uchio Kondo <udzura@udzura.jp>
2020-06-26 18:30:12 +0900

    Add Finfo#unix_mode helper to express UNIX-ish mode_t

--- mrbgems/mod_mruby_mrblib/mrblib/mrb_apache.rb
+++ mrbgems/mod_mruby_mrblib/mrblib/mrb_apache.rb
@@ -21,6 +21,21 @@ class Apache
       self.insert_eos
     end
   end
+  # "%x" % permission #=> "744"
+  # "%o" % unix_mode  #=> "744"
+  class Finfo
+    def unix_mode
+      apr_perm = self.permission
+      mask = 0b1111
+      unix_mask = 0b111
+      omode = (apr_perm & (0b1111)) & unix_mask
+      gmode = (apr_perm & (0b1111 << 4)) >> 4 & unix_mask
+      umode = (apr_perm & (0b1111 << 8)) >> 8 & unix_mask
+
+      omode | (gmode << 3) | (umode << 6)
+    end
+    alias mode unix_mode
+  end
 end
 module Kernel
   def get_server_class
